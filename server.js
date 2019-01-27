8d288df10b4db6a7dcef83b931e26989ce4cda80
Melkisedek Hamakali <melkisedek.hamakali@gmail.com>
2019-01-27 22:24:55 +0200

    Return json on file upload

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
-
-// require and use "multer"...
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
@@ -11,11 +11,19 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/hello', function (req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+  res.json({
+    "name": req.file.originalname,
+    "type": req.file.mimetype,
+    "size": req.file.size
+  });
 });
 
 app.listen(process.env.PORT || 3000, function () {
